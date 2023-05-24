32cd85e76c4b9ad6a5987ca2af2ef08fbba14669
Ygal Blum <ygal.blum@gmail.com>
2023-05-24 14:03:01 +0300

    Split to two tests

--- repos/system_upgrade_supplements/common/actors/simple/tests/component_test_simple.py
+++ repos/system_upgrade_supplements/common/actors/simple/tests/component_test_simple.py
@@ -2,13 +2,21 @@ import mock
 from leapp.libraries.actor import simple
 
 
-def test_actor_lib_some_function(current_actor_context):
+def test_from_actor(current_actor_context):
     with mock.patch("leapp.libraries.actor.simple._subprocess_call") as m:
         current_actor_context.run()
-        simple.run_non_existing_bin("from_test")
         m.assert_has_calls(
             calls=[
                 mock.call(["/opt/non_existing/bin/run", "from_actor"]),
+            ]
+        )
+
+
+def test_from_test():
+    with mock.patch("leapp.libraries.actor.simple._subprocess_call") as m:
+        simple.run_non_existing_bin("from_test")
+        m.assert_has_calls(
+            calls=[
                 mock.call(["/opt/non_existing/bin/run", "from_test"])
             ]
         )
